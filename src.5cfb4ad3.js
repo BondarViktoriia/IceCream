deabd9267a173e985e4f102079f72516a39edf3c
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2022-07-14 12:29:57 +0000

    Deploying to gh-pages from @ BondarViktoriia/IceCream@db9c1f184b404bbe0959ecf8c36cda0fc75275ad 🚀

--- src.5cfb4ad3.js
+++ src.5cfb4ad3.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-1ver/src.3e2b6fa3.js.map
+//# sourceMappingURL=/IceCream/src.5cfb4ad3.js.map
